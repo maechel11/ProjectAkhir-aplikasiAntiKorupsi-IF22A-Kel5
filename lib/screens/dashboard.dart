3002cf727bd038c2e126590e5b025fea957bb506
maechel11 <155069046+maechel11@users.noreply.github.com>
2024-05-28 23:33:29 +0700

    Add files via upload

---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:snow_login/screens/homescreen.dart';
+
+class dashboard extends StatefulWidget {
+  const dashboard({super.key});
+
+  @override
+  State<dashboard> createState() => _dashboardState();
+}
+
+class _dashboardState extends State<dashboard> {
+
+int _selectedIndex = 0;
+  String _title = '';
+  List<Widget> _widgetOptions = <Widget>[
+    homescreen()
+  ];
+
+  @override
+   void initState(){
+    super.initState();
+
+    _title = 'default';
+  }
+
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        child: AppBar(
+          elevation: 2,
+          toolbarHeight: 100,
+          backgroundColor: Colors.blue,
+          centerTitle: true,
+          title: _title == 'default' ? Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget> [
+                Flexible(
+                  flex: 1,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Image.asset(
+                            '../asset/images/logo2.png',
+                            width: 150,
+                            height: 150,
+                          ),
+                          // Text(
+                          //   'RAT',
+                          //  overflow: TextOverflow.ellipsis,
+                          //         maxLines: 1,
+                          //         style: TextStyle(
+                          //           fontSize: 20,
+                                    
+                                 
+                          //           color: Colors.black,
+                          //           fontWeight: FontWeight.w900
+                          //         ),
+                          // ),
+                          // // Text(
+                          //         'User',
+                          //         overflow: TextOverflow.ellipsis,
+                          //         maxLines: 1,
+                          //         style: TextStyle(
+                          //           fontSize: 12,
+                          //           color: Colors.black,
+                          //         ),
+                          //       ),
+                      
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                CircleAvatar(
+                        radius: 30,
+                        child: CircleAvatar(
+                          radius: 27,
+                          backgroundImage:
+                          AssetImage('../asset/images/profile.jpeg'),
+                          backgroundColor: Colors.amber,
+                        ),
+                      ),
+              ],
+            ),
+          )
+           : Text(_title,
+          maxLines: 1,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+        ),
+      )
+        ),
+        preferredSize: Size.fromHeight(70.0),
+      ),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30.0),
+          topRight: Radius.circular(30.0),
+        ),
+                child: BottomNavigationBar(
+          onTap: onTabTapped,
+          currentIndex: _selectedIndex,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          unselectedItemColor: Colors.blue,
+          selectedItemColor: Colors.red,
+          items: [
+            BottomNavigationBarItem(
+              label: "Beranda",
+              icon: Icon(Icons.home),
+            ),
+            BottomNavigationBarItem(
+              label: "Jelajahi",
+              icon: Icon(Icons.explore),
+            ),
+            BottomNavigationBarItem(
+              label: "Kategori",
+              icon: Icon(Icons.category),
+            ),
+            BottomNavigationBarItem(
+              label: "Profil",
+              icon: Icon(Icons.people),
+            ),
+          ],
+        ),
+      ),
+      body: _widgetOptions.elementAt(_selectedIndex),
+    );
+  }
+  void onTabTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+      switch (index) {
+        case 0:
+          {
+            _title = 'default';
+          }
+          break;
+        case 1:
+          {
+            _title = 'navbar 1';
+          }
+          break;
+        case 2:
+          {
+            _title = 'navbar 2';
+          }
+          break;
+        case 3:
+          {
+            _title = 'navbar 3';
+          }
+          break;
+      }
+    });
+  }
+}
---
+++ lib/screens/laporandata.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LaporanData extends StatefulWidget {
+  const LaporanData({super.key});
+
+  @override
+  State<LaporanData> createState() => _LaporanDataState();
+}
+
+class _LaporanDataState extends State<LaporanData> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
