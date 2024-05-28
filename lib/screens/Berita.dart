e3212d12bca35b8f52f9d6d2c674a07aeb04b334
maechel11 <155069046+maechel11@users.noreply.github.com>
2024-05-28 23:32:10 +0700

    Delete lib directory

--- lib/screens/Berita.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Berita {
-  final String imagePath;
-  final String judul;
-  final String tanggal;
-
-  Berita(this.imagePath, this.judul, this.tanggal);
-}
